b2aa94753348b97240ea3eeba1c82939faafe4f1
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2023-03-09 20:28:30 +0100

    Jump only in ground

--- Assets/jump.cs
+++ Assets/jump.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class jump : MonoBehaviour
 {
+    public bool isOnGround = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +15,16 @@ public class jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround){
             Debug.Log("Espacio Pulsado");
             this.GetComponent<Rigidbody>().AddForce(Vector3.up * 200);
+            isOnGround = false;
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground")){
+            isOnGround =  true;
+        }        
     }
 }
