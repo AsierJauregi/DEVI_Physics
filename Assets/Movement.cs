af77361f77111e304010c25573ebb5b25750a121
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2023-03-10 09:23:32 +0100

    Layer wall añadido + tile destrucción

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -16,26 +16,22 @@ public class Movement : MonoBehaviour
     private float zInput;
     void Update()
     {
-        /*if(Input.GetKeyDown(KeyCode.UpArrow)){
-            Debug.Log("Flecha Arriba Pulsada");
-            );
-        }
-        if(Input.GetKeyDown(KeyCode.DownArrow)){
-            Debug.Log("Flecha Abajo Pulsado");
-            transform.position += new Vector3(-1 * speed * Time.deltaTime, 0, 0);
-        }
-        if(Input.GetKeyDown(KeyCode.LeftArrow)){
-            Debug.Log("Flecha Izquierda Pulsado");
-            transform.position += new Vector3(0, 0, -1 * speed * Time.deltaTime);
-        }
-        if(Input.GetKeyDown(KeyCode.RightArrow)){
-            Debug.Log("Flecha Derecha Pulsado");
-            transform.position += new Vector3(0, 0, 1 * speed * Time.deltaTime);
-        }*/
         xInput = Input.GetAxis("Horizontal");
         zInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * zInput);
         transform.Translate(Vector3.right * Time.deltaTime * speed * xInput);
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.Layer == LayerMask.LayerToName("Wall")){
+            Debug.Log("Collision con pared");
+        }  
+
+        if(collision.gameObject.Layer == LayerMask.LayerToName("Death Tile")){
+            Debug.Log("Capsule destroyed");
+            Destroy(this.gameObject);
+        }      
     }
 }
