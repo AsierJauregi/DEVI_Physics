b22cc7ae4fe2caf02808ecb4cbd2cb1a28eee6b6
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2023-03-23 09:07:42 +0100

    Raycast added to scene

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -7,6 +7,8 @@ public class Shooting : MonoBehaviour
     [SerializeField]
     private float force = 5000;
     public GameObject bullet;
+    private int raycastTarget;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +19,26 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.F)){
             GameObject newBullet = Instantiate(bullet);
             newBullet.transform.position = this.gameObject.transform.position;
             newBullet.GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
+        }
+    }
+    private void FixedUpdate()
+    {
+        raycastTarget = 1 << 10;
+        raycastTarget = ~raycastTarget;
+        RaycastHit hit;
+
+        if (Physics.Raycast(this.gameObject.transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, raycastTarget))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            Debug.Log("Is hitting Layer " + LayerMask.LayerToName(hit.collider.gameObject.layer));
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
         }
     }
 }
